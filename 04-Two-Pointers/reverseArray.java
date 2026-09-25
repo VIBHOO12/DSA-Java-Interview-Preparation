533f211820c4a20901a827baba64d7bf5560b228
Vibhoo Vishwakarma <dipshikhavishwakarma0@gmail.com>
2026-09-25 20:37:47 +0530

    Two pointer problems

--- 04-Two-Pointers/reverseArray.java
+++ 04-Two-Pointers/reverseArray.java
@@ -1,6 +1,6 @@
 public class reverseArray{
     public static void main(String[] args){
-        int[] arr = {1,2,3,4,5};
+        int[] arr = {1,2,3,4,5,6};
         int left=0; 
         int right = arr.length-1;
 
